a73cd3f97deb5ee056231ff6ec3221e4c30b6999
466656936 <466656936@qq.com>
2017-09-25 20:12:48 +0800

    将MyConnection的ip连接地址从127.0.0.1改为172.18.23.6

--- src/main/java/com/yh/util/MyConnection.java
+++ src/main/java/com/yh/util/MyConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-	private final static String DBURL="jdbc:mysql://127.0.0.1:3306/wy?useUnicode=true&characterEncoding=utf8";
+	private final static String DBURL="jdbc:mysql://172.18.23.6:3306/wy?useUnicode=true&characterEncoding=utf8";
 	private final static String USERNAME="root";
 	private final static String PASSWORD="123";
 	private final static String DBDRIVER="com.mysql.jdbc.Driver";
